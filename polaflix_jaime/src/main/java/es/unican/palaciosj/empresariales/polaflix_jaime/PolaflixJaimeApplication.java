11f1adb8ef12fe86f935f6425cba68964b1877a2
JaimePalM <jaimepalacios240@gmail.com>
2023-03-03 18:19:02 +0100

    Initial commit

---
+++ polaflix_jaime/src/main/java/es/unican/palaciosj/empresariales/polaflix_jaime/PolaflixJaimeApplication.java
@@ -0,0 +1,13 @@
+package es.unican.palaciosj.empresariales.polaflix_jaime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolaflixJaimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolaflixJaimeApplication.class, args);
+	}
+
+}
---
+++ polaflix_jaime/src/test/java/es/unican/palaciosj/empresariales/polaflix_jaime/PolaflixJaimeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unican.palaciosj.empresariales.polaflix_jaime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolaflixJaimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
